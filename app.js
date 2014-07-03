bf995055447f3cac6df2c4992b6b5b8fb073b285
quyatong <quyatong@126.com>
2014-07-04 01:04:45 +0800

    submit

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 9090);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
